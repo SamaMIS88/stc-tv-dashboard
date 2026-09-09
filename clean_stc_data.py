b21c394b44d85655124fce1f4cd4d60002b22122
Sama <samaalshehri817@gmail.com>
2026-09-09 22:47:26 +0300

    add gitignore rule for pkl files

--- clean_stc_data.py
+++ clean_stc_data.py
@@ -1,3 +1,10 @@
-import pandas as pd 
-df = pd.read_excel('stc_raw_events.xlsb', engine='pyxlsb')
-print(df.shape) 
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+
+df = pd.read_pickle('events_raw.pkl')
+
+df['date_'] = pd.to_datetime(df['date_'], unit='D', origin='1899-12-30')
+df['duration_hours'] = df['duration_seconds'] / 3600
+
+print(df[['date_', 'duration_seconds', 'duration_hours']].head(10))
