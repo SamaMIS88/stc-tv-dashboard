87cbc23ac7c5e9620071c53c27856f0a55f08767
Sama <samaalshehri817@gmail.com>
2026-09-09 23:09:16 +0300

    add flag conversion and whitespace trimming

--- clean_stc_data.py
+++ clean_stc_data.py
@@ -8,3 +8,15 @@ df['date_'] = pd.to_datetime(df['date_'], unit='D', origin='1899-12-30')
 df['duration_hours'] = df['duration_seconds'] / 3600
 
 print(df[['date_', 'duration_seconds', 'duration_hours']].head(10))
+is_movie = df['program_class'] == 'MOVIE'
+df.loc[is_movie, ['season', 'episode']] = None
+print(df[df['program_class'] == 'MOVIE'][['program_class', 'season', 'episode']].head())
+df['hd'] = df['hd'].astype(bool)
+df['series_title'] = df['series_title'].astype(bool)
+print(df[['hd', 'series_title']].head(10))
+df['program_name'] = df['program_name'].str.strip()
+df['program_class'] = df['program_class'].str.strip()
+df['program_desc'] = df['program_desc'].str.strip()
+df['program_genre'] = df['program_genre'].str.strip()
+df['original_name'] = df['original_name'].str.strip()
+print(df[['program_name', 'program_class', 'program_desc', 'program_genre', 'original_name']].head(10))
